5fe0e499804d1b6df7e9db140a59d17def3110e8
Rani Shah <rani76@gmail.com>
2018-02-26 17:47:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(number) 
+def fizzbuzz(number)
   if (num % 3 == 0) &&  (num % 5 == 0)
     return "FizzBuzz"
-  elsif num % 3 == 0 
+  elsif num % 3 == 0
     return "Fizz"
-  elsif num % 5 == 0 
+  elsif num % 5 == 0
     return "Buzz"
-  else 
-  return nil 
-end 
+  else
+  return nil
+end
 end
