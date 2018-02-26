9326e90c08740b6335af694e3074bc89b16cb61d
Rani Shah <rani76@gmail.com>
2018-02-26 17:50:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
   if num % 15 == 0
-    puts "FizzBuzz"
+   "FizzBuzz"
   elsif num % 3 == 0
-    puts "Fizz"
+     "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+     "Buzz"
   else
-  return nil
+  "nil"
 end
 end
